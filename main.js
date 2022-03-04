6d89edd09acaa313c72d48937c9d3e7ac414e2b2
Jacob Whiteheart <77899874+jhwhiteheart@users.noreply.github.com>
2022-03-04 16:40:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var home = {
+	"name": "Home",
+	"index": 1,
+}
+
+var pages = [
+	{
+		"name": "Home",
+		"contents": "content"
+	},
+	{
+			"name": "Contact",
+			"contents": "CONTENT2"
+	}
+]
+
+pages[0].content
